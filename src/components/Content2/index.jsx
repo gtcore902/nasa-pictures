8afd4ec70fc37b2b4cf5e38ff5dbb0210204554d
gtcore902 <gaetan.tremois@gmail.com>
2024-05-13 15:57:45 +0200

    feat: set classes for filter buttons

--- src/components/Content2/index.jsx
+++ src/components/Content2/index.jsx
@@ -3,6 +3,7 @@ import { API_KEY } from '../../API_KEYS';
 
 const Content2 = () => {
   const [datas, setDatas] = useState([]);
+  const [filteredDatas, setFilteredDatas] = useState([]);
   const [pictures, setPictures] = useState([]);
   const [picturesFirstCol, setPicturesFirstColumn] = useState([]);
   const [picturesSecondCol, setPicturesSecondColumn] = useState([]);
@@ -24,7 +25,6 @@ const Content2 = () => {
       console.log();
       if (response.status === 200) {
         setDatas(datas.latest_photos);
-        // console.log(response);
         console.log(datas);
         console.log(datas);
       }
@@ -48,7 +48,8 @@ const Content2 = () => {
    */
   useEffect(() => {
     const picturesArray = [];
-    datas.map((data) => picturesArray.push(data.img_src));
+    setFilteredDatas(datas);
+    filteredDatas.map((data) => picturesArray.push(data.img_src));
     setPictures(picturesArray);
     setPicturesFirstColumn(pictures.slice(0, Math.ceil(pictures.length / 3)));
     setPicturesSecondColumn(
@@ -58,20 +59,42 @@ const Content2 = () => {
       )
     );
     setPicturesLastCol(pictures.slice(Math.ceil(pictures.length / 3) * 2));
-  }, [datas]);
+  }, [datas, filteredDatas]);
 
   /**
    * Set new set for all cameras
    */
   useEffect(() => {
     const camList = new Set();
-    datas.filter((data) => camList.add(data?.camera?.name));
-    setListCam(camList);
+    datas.filter((data) => camList.add(data?.camera?.full_name));
+    setListCam(['All'].concat(...camList));
   }, [datas]);
+
+  const sortCam = (event) => {
+    if (event.target.id === 'All') {
+      setFilteredDatas(datas.filter((data) => data));
+    } else if (event.target.id !== 'All') {
+      setFilteredDatas(
+        datas.filter((data) => data.camera?.full_name === event.target.id)
+      );
+    }
+  };
 
   return (
     <div>
       <p className="text-center">Date : {datas[0]?.earth_date}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-2 my-8 md:mx-32 md:my-8 max-w-[1920px]">
+        {Array.from(listCam).map((camera, index) => (
+          <button
+            key={index}
+            className="w-auto text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:text-white focus:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            id={camera}
+            onClick={(event) => sortCam(event)}
+          >
+            {camera}
+          </button>
+        ))}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 items-start md:text-left mx-2 my-8 md:mx-32 md:my-8 max-w-[1920px]">
         <div className="grid grid-cols-1 gap-2">
           {picturesFirstCol.map((picture, index) => (
