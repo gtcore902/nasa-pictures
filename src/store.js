7c06baae7486097f4cdb366bdad8fe92271fbd99
gtcore902 <gaetan.tremois@gmail.com>
2024-05-15 18:46:54 +0200

    refactor: remove comments

--- src/store.js
+++ src/store.js
@@ -7,24 +7,6 @@ let state = {
 
 const reducer = (currentState, action) => {
   switch (action.type) {
-    // case 'ADD_PRODUCT':
-    //   const listWithNewProduct = [...currentState.list, action.payload];
-    //   return { ...currentState, list: listWithNewProduct };
-    // case 'REMOVE_PRODUCT':
-    //   const list = currentState.list.filter(
-    //     (item, index) => index !== action.payload
-    //   );
-    //   return { ...currentState, list: list };
-    // case 'APPLY_VOUCHER':
-    //   const withVoucherList = currentState.list.map((item) =>
-    //     item.title === 'Super Crémeux'
-    //       ? { ...item, price: action.payload.price }
-    //       : item
-    //   );
-    //   return { ...currentState, list: withVoucherList };
-    // case 'UPDATE_FIRSTNAME':
-    //   const owner = { ...currentState.owner, firstName: action.payload };
-    //   return { ...currentState, owner };
     case 'IS_CONNECTED':
       return { ...currentState, connectedState: action.payload };
     default:
