b48dc8c51a148d1e23dbdd57216df030935c53d4
gtcore902 <gaetan.tremois@gmail.com>
2024-05-23 14:19:04 +0200

    feat: adjust grid styles

--- src/components/Content2/index.jsx
+++ src/components/Content2/index.jsx
@@ -107,7 +107,7 @@ const Content2 = () => {
         {Array.from(listCam).map((camera, index) => (
           <button
             key={index}
-            className="w-auto text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:text-white focus:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            className="first-of-type:col-span-2 md:first-of-type:col-auto w-auto text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:text-white focus:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
             id={camera}
             onClick={(event) => sortCam(event)}
           >
@@ -122,8 +122,8 @@ const Content2 = () => {
           alt="Rover illustration"
         />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px] items-start md:text-left mx-2 my-8 md:mx-32 md:my-8">
-          <div className="grid grid-cols-1 gap-[24px]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-2 gap-[24px] items-start md:text-left mx-2 my-8 md:mx-32 md:my-8">
+          <div className="grid grid-cols-1 gap-2 md:gap-[24px]">
             {picturesFirstCol.map((picture, index) => (
               <div key={index} className="">
                 <img
@@ -135,7 +135,7 @@ const Content2 = () => {
               </div>
             ))}
           </div>
-          <div className="grid grid-cols-1 gap-[24px]">
+          <div className="grid grid-cols-1 gap-2 md:gap-[24px]">
             {picturesSecondCol.map((picture, index) => (
               <div key={index} className="">
                 <img
@@ -147,7 +147,7 @@ const Content2 = () => {
               </div>
             ))}
           </div>
-          <div className="grid grid-cols-1 gap-[24px]">
+          <div className="grid grid-cols-1 gap-2 md:gap-[24px]">
             {picturesLastCol.map((picture, index) => (
               <div key={index} className="">
                 <img
