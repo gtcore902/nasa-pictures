5ce613967e5f1f476fe2054825bf4951d8b5fdbf
gtcore902 <gaetan.tremois@gmail.com>
2024-06-16 21:06:14 +0200

    feat: change footer icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faDesktop, faRocket } from '@fortawesome/free-solid-svg-icons';
+
 const Footer = ({ style }) => {
   return (
     <div className={style}>
-      <p>
-        Thanks to <a href="https://api.nasa.gov/">api.nasa.gov</a>
+      <p className="my-4">
+        Thanks to{' '}
+        <a href="https://api.nasa.gov/">
+          api.nasa.gov
+          <FontAwesomeIcon icon={faRocket} className="mx-2 text-gray-800" />
+        </a>
       </p>
-      <p>
-        Made with 💻 by{' '}
-        <a href="https://gaetantremois.fr"> Gaëtan TREMOIS 🚀</a>
+      <p className="my-4">
+        Made with
+        <FontAwesomeIcon icon={faDesktop} className="mx-2 text-gray-800" />
+        by <a href="https://gaetantremois.fr"> Gaëtan TREMOIS</a>
+      </p>
+      <p className="my-4">
+        Fork me on{' '}
+        <a href="https://github.com/gtcore902/nasa-pictures">
+          GitHub
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="lg"
+            className="mx-2 text-gray-800"
+          />
+        </a>
       </p>
     </div>
   );
